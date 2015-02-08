7390c76984804a4c700810ef0cb0c5e7c2a0be7e
Mohammed Sohaib Majeed <mohammedsohaib786@hotmail.co.uk>
2015-02-08 22:40:24 +0000

    Website Update (08-02-2015)
    Website Update (08-02-2015)

--- header.php
+++ header.php
@@ -1,31 +1,58 @@
 <!DOCTYPE html>
 <head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<link rel="stylesheet" type="text/css" href="admin.css">
-		<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	<title>BUWFC | Home</title>
+	<link rel="shortcut icon" href="favicon.ico" type="favicon.ico" />
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/custom.css">
+	<link rel="stylesheet" type="text/css" href="css/admin.css">
+
+	<!-- Bootstrap -->
+	<link href="scripts/carousel/style.css" rel="stylesheet" type="text/css">
+	<link href="scripts/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="scripts/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet">
+
+	<!-- Fonts -->
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+	<link href='http://fonts.googleapis.com/css?family=Slabo+27px' rel='stylesheet' type='text/css'>
+	<link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Palatino+Linotype" rel="stylesheet" type="text/css">
+    
+	<!-- Icons -->
+    <link href="scripts/icons/general/stylesheets/general_foundicons.css" media="screen" rel="stylesheet" type="text/css" />  
+    <link href="scripts/icons/social/stylesheets/social_foundicons.css" media="screen" rel="stylesheet" type="text/css" />
+
 </head>
 
 <body>
-	
-	<div id="banner">
-	<p class="logo"><a class="logo2" href="index.php">BUWFC</a></p>
-	<p class="abovelogo"><a>BRADFORD UNIVERSITY WOMENS FOOTBALL CLUB</a><p>
-	
+
+<div id="decorative2">
+    <div class="container">
+
+        <div class="divPanel topArea notop nobottom">
+            <div class="row-fluid">
+                <div class="span12">
+
+                    <div id="divLogo" class="pull-left">
+                        <a href="index.php" id="divSiteTitle">BUWFC</a><br />
+                        <a href="index.php" id="divTagLine">BRADFORD UNIVERSITY WOMANS FOOTBALL CLUB</a>
+                  </div>
+
 	<ul>
-	
 	<a class="menuItem" href="matchReport.php"><li class="button" id="mainButtons"><center>Match Report</center></li></a>
 	<a class="menuItem" href="socialEvents.php"><li class="button" id="mainButtons"><center>Social Events</center></li></a>
 	<a class="menuItem" href="profiles.php"><li class="button" id="mainButtons"><center>Profiles</center></li></a>
 	<a class="menuItem" href="index.php"><li class="button" id="mainButtons"><center>Home</center></li></a>
 	<a class="menuItem" href="login.php"><li class="button" id="mainButtons"><center>Admin Login</center></li></a>
 	</ul>
-	
-	
-	
-	</div>
-	<div id="container">
-		<!--CONTENT -->
-		
-	
-		
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- CONTENT START -->
+<div id="container">
