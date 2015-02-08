cca4db838e802fe62b3624bb51d8f20287428fe2
Mohammed Sohaib Majeed <mohammedsohaib786@hotmail.co.uk>
2015-02-08 22:49:58 +0000

    Website Update (08/02/2015)
    Website Update (08/02/2015)

---
+++ footer.php
@@ -0,0 +1,38 @@
+</div> <!-- CONTENT END -->
+<div id="footerOuterSeparator"></div>
+
+<div id="divFooter" class="footerArea">
+
+    <div class="container">
+
+        <div class="divPanel">
+
+            <div class="row-fluid">
+                <div class="span12">
+                    
+                    <p class="copyright">
+                        Copyright © 2015 Pi Society - University of Bradford. All Rights Reserved.
+                    </p>
+
+                    <p class="social_bookmarks">
+            <a href="https://www.facebook.com/pages/Bradford-University-Womens-Football-Club/1397797237101160"><i class="social foundicon-facebook"></i> Facebook</a>
+			<a href="https://twitter.com/team_buwfc"><i class="social foundicon-twitter"></i> Twitter</a>
+            <a href="https://twitter.com/ubupisoc"><i class="social foundicon-twitter"></i> Pi Society</a>
+                    </p>
+
+                </div>
+            </div>
+            <br/>
+        </div>
+    </div>
+</div>
+
+<script src="scripts/jquery.min.js" type="text/javascript"></script> 
+<script src="scripts/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="scripts/default.js" type="text/javascript"></script>
+
+<script src="scripts/carousel/jquery.carouFredSel-6.2.0-packed.js" type="text/javascript"></script>
+<script type="text/javascript">$('#list_photos').carouFredSel({ responsive: true, width: '100%', scroll: 2, items: {width: 320,visible: {min: 2, max: 6}} });</script>
+
+</body>
+</html>
