8e89e5a1a58b55bfdd7e954ac775cc3872a1c669
Sumaia Sabouni <sumaia.sabouni@gmail.com>
2015-02-09 12:16:55 +0000

    Linked editProfile to admin page

--- profiles.php
+++ profiles.php
@@ -9,7 +9,7 @@
 	if($response){
 		while($row = mysqli_fetch_array($response)){
 			echo '<div class="profileBox"><center> <p> 
-			<div class="placeHolderPic"><img src="eiffel.jpg" alt="" /></div> <br />' 
+			<div class="placeHolderPic"><img src="img/eiffel.jpg" alt="" /></div> <br />' 
 			.'Name: '. $row['Name'] . '<br />' .'DOB: '. $row['Birthday'] 
 			. '<br />' .'Course: '. $row['Course'] . '<br />' .'Year of Study: '. $row['YearOfStudy'] . '<br />' . 'Years of Activity: '.$row['YearsOfActivity'] . 
 			'<br />' . 'Position: '.$row['Position'] . '<br />' .'Tag: '. $row['Tag'] . '<br />' .'Hobbies: '. $row['Hobbies'] . '<br />' . '</p></center> </div>';
