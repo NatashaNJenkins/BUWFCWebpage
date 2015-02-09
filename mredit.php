38ca326e62b52671b3716b1dadbb911028bc1f15
Natasha Micic <natasha.micic@yahoo.co.uk>
2015-02-09 12:10:28 +0000

    Now connects

--- mredit.php
+++ mredit.php
@@ -1,37 +1,54 @@
-<html>
-<head>
-	<title>BUWFC</title>
-	<link rel="stylesheet" href="styles.css">
-</head>
-<body>
-<div id="container">
+<?php
 
+include "header.php";
+include "functions.php";
+checkUser();
 
-<?php session_start();
+if (isset($_POST["sub"])){
 
+  $date = $_POST["date"];
+  $loc = $_POST["location"];
+  $opp = $_POST["opposition"];
+  $man = $_POST["man"];
+  $mup = $_POST["muppet"];
+  $rep = $_POST["report"];  
+  
 
-if(!isset($_SESSION['user'])){
-  header("login.html");
-  exit();
+  $conn = connectToDb();
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-else{
-   if(isset($_POST['mredit'])){
-     
-     $mredit=$_POST['mredit'];
 
-   echo 'Welcome, '.$_SESSION['user'].', to your match reports editing area.';
+$sql = "INSERT INTO `Match` (`Date`, `Location`, `Opposition`,`Man_of_Match`,`Muppet_of_Match`, `Report`)
+                   VALUES ('".$date."', '".$loc."', '".$opp."', '".$man."','".$mup."','".$rep."')";
 
-   }
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close(); 
+
+
 }
 
 ?>
 
-<form action="matchcheck.php" method="POST">
-  Date:<br>
-  <input type=
+<form action="" method="POST">
+  Date  <input type="text" name="date"></input> <br><br>
+  Location  <input type="text" name="location"></input> <br><br>
+  Opposition  <input type="text" name="opposition"></input> <br><br>
+  Man of The Match  <input type="text" name="man"></input> <br><br>
+  Muppet of The Match  <input type="text" name="muppet"></input> <br><br>
+  Report  <textarea  name="report" lable="" rows="20" cols="60"> Enter report details here!
+</textarea><br><br>
+  <input type="submit" name="sub" value="Save and Send"></input>
+</form>
 
+<?php
 
-</div>
+  include "footer.php";
 
-</body>
-</html>
+?>
